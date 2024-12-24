efac17a29154ef911cf6e335eb99c89b4dd98523
rogelioConsejo <rogelio.consejo@gmail.com>
2024-12-24 17:06:44 -0600

    refactor: better comments

--- payment/method.go
+++ payment/method.go
@@ -2,7 +2,9 @@ package payment
 
 import "errors"
 
+// Method is the interface that all payment methods must implement.
 type Method interface {
+	// Validate checks if the payment is valid and can be processed by the payment method (amount, currency, etc).
 	Validate(Payment) error
 	// Create may pre-authorize the payment if needed, but it should not charge the payment yet.
 	Create(Payment) (ID, error)
